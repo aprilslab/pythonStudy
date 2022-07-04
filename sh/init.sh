5750336b5ef9fafdc8ccf8abb024064a9d605988
aprilslab <april.kim412@gmail.com>
2022-07-04 18:01:57 +0000

    Updated init.sh

--- sh/init.sh
+++ sh/init.sh
@@ -1,6 +1,29 @@
 #!/bin/bash
 
-#jupyter notebook --generate-config
+###########
+# Git Settings
+###########
+
+chmod 777 -R private/
+git config --global user.email "april.kim412@gmail.com"
+git config --global user.name "aprilslab"
+git config user.email "april.kim412@gmail.com"
+git config user.name "aprilslab"
+git config --global init.defaultBranch main
+git config --global credential.helper store #'store --file private/.git-credentials'
+git config core.quotepath false
+
+
+###########
+# jupyter Settings
+###########
+
+FILE=/home/runner/.jupyter/jupyter_notebook_config.py
+if [ -f "$FILE" ]; then
+    echo "$FILE exists."
+else 
+    jupyter notebook --generate-config
+fi
 
 echo '{
   "NotebookApp": {
